7e7ae55f13da86425d40065bb6bb268ada8bc62c
saltybirder <james.m.salinas@gmail.com>
2024-01-15 20:59:47 -0500

    added def for name, hours and rate

--- CIS261_Course_Project.py
+++ CIS261_Course_Project.py
@@ -2,4 +2,20 @@
 #CIS261
 #Week 2 CIS261 Course Project- Payrole
 
+#This function will return and get the employee name
+def getEmpName():
+    empName = input('Enter employee name:    ')
+    return empName
 
+#Enter and get hours
+def getHoursWorked():
+    hours = float(input('Enter hours worked:     '))
+    return hours
+
+#Enter and get rate of pay
+def getPayRate():
+    payRate = float(input('Enter rate of pay:     '))
+    return payRate
+
+
+    
